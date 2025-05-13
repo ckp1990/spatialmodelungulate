f7b0bf5c014162f535c13db8a0733eb76d286773
chandanp <chandanpandey0404@gmail.com>
2025-05-13 16:28:21 +0530

    May13

--- Test and learn/NIMBLE learn.R
+++ Test and learn/NIMBLE learn.R
@@ -1,2 +1,7 @@
-## basic fitting of detection function using NIMBLE
+# Basic fitting of detection function using NIMBLE
+
+## Simple Linear regression model in R using BUGS lang in NIMBLE
+
+library(nimble)
+?nimbleCode
 
